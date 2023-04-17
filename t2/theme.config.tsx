363344861c770c5f185aefa9a59612021f8d3a2f
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-04-17 18:40:27 +0000

    🌐

--- t2/theme.config.tsx
+++ t2/theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/sniffowomo',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/sniffowomo',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'SniffTest',
   },
 }
 
